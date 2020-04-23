b1ea0666cde2fe64e331faa2501c9e935a67a3ba
Terebo <63092966+Terebo@users.noreply.github.com>
2020-04-23 10:37:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set up stuff
+            ConsoleKeyInfo key;
+            Boolean stop = false;
+            Boolean WantingToStop = false;
+            //write instruction
+            Console.WriteLine("Press esc to exit \n");
+            do
+            {
+                key = Console.ReadKey(true);
+                //look for the pressing of the esc key
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    //ask for confirmation
+                    Console.WriteLine("Are you sure? \n Y/N");
+                    do
+                    {
+                            //for some weird reason i need to record the character again....
+                            key = Console.ReadKey(true);
+                            //makes it so this thing doesn't fire 200000000000000 times
+                            WantingToStop = true;
+                            //looking for Y, if so end the program
+                            if (key.Key.ToString() == "Y")
+                            {
+                                Environment.Exit(0);
+                            }
+                            //if N continue and exit the exit cycle
+                            if (key.Key.ToString() == "N")
+                            {
+                                WantingToStop = false;
+                                continue;
+                            }
+                            //if neither Y or N, tell the user
+                        if (key.Key != ConsoleKey.Escape && key.Key.ToString() != "J" && key.Key.ToString() != "N")
+                        {
+                            Console.WriteLine(key.Key.ToString() + " is not a valid response");
+                        }
+                    } while (WantingToStop == true);
+                }
+            } while (stop != true);
+        }
+    }
+}
